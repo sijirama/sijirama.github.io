fd1edbcee0858c23633a0a230d1d2b8fae173983
sijibomi <gbemilesanmi@gmail.com>
2023-03-19 00:20:06 +0100

    feature: Navbar and Hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,12 +11,12 @@ export default function Hero({}: Props) {
       <div className="max-h-[500px] relative">
 
         {/*Overlay*/}
-        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/50 flex flex-col justify-center ">
+        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/70 flex flex-col justify-center ">
             <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold '>The <span className='text-orange-500 '>Worst</span></h1>
             <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold '><span className='text-orange-500 '>Meals </span>Prepared</h1>
         </div>
 
-        <img className="" src={ImageUrl} alt="" />
+        <img className="w-full max-h-[500px] object-cover" src={ImageUrl} alt="" />
 
       </div>
     </div>
