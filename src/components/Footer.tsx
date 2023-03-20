425e242f8f4a404cfd5ab18cdfd47e08e8d087e7
sijibomi <gbemilesanmi@gmail.com>
2023-03-20 01:42:04 +0100

    feature: footer, typography

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,22 +10,22 @@ function Footer({}: Props) {
       <div className="w-full lg:w-5/6 h-5/6 flex flex-col md:flex-row bg-gray-800 mx-auto text-white">
 
         {/* Left Side */}
-        <div className='w-full md:w-1/2 flex flex-col gap-10 px-5 py-5 md:px-10 md:py-y justify-center align-center'>
-          <div className="font-Abril text-lg md:text-3xl">WorstEats</div>
+        <div className='w-full md:w-1/2 flex flex-col gap-10 px-5 py-5 md:px-10 md:py-y justify-center align-center md:border-r-1 border-white'>
+          <div className="font-Abril text-xl md:text-3xl">WorstEats</div>
 
           <div className=' font-Almarai grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4'>
             <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
-            <p>Careers</p>
+            <p>Consumer care</p>
+            <p>WorstEat News</p>
+            <p>Newsletter</p>
+            <p>Address</p>
+            <p>Updates</p>
+            <p>Alumni</p>
+            <p>Our Cafe</p>
           </div>
 
-          <div className='flex gap-5'>
-            <img src='/socials/github.png' alt='' />
+          <div className='flex gap-5 p-2 md:w-1/2 justify-center bg-white'>
+            <img src='/socials/github.png' alt='' className='' />
             <img src='/socials/whatsapp.png' alt='' />
             <img src='/socials/linkedin.png' alt='' />
             <img src='/socials/instagram.png' alt='' />
@@ -37,12 +37,12 @@ function Footer({}: Props) {
         <div className='w-full md:w-1/2 flex flex-col gap-10 align-center px-5 py-5 md:px-10 md:py-y justify-center align-center font-Almarai'>
           <p>Get the Worst Meals Ever</p>
           <div className='flex'>
-            <input type='text' placeholder='' />
-            <button>subscribe</button>
+            <input className="bg-transparent outline-none px-2 border-white border-2"  type='text' placeholder='' />
+            <button className='rounded-none border-1 border-white'>subscribe</button>
           </div>
 
           <div className='flex gap-2'>
-            <input type="checkbox" />
+            <input className='accent=pink-500' type="checkbox" />
             <p className='font-Almarai'>By checking the box you agree that your parents dont love you.</p>
           </div>
 
